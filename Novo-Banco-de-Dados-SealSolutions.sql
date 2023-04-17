68880cad813b991b461e171f24e17c05c0c4763e
RafaAR2 <rafael.areis@sptech.school>
2023-04-16 23:02:09 -0300

    Atualização BD e Diagrama

--- Novo-Banco-de-Dados-SealSolutions.sql
+++ Novo-Banco-de-Dados-SealSolutions.sql
@@ -2,75 +2,58 @@ CREATE DATABASE sealSolutions;
 
 USE sealSolutions;
 
-CREATE TABLE tipo_usuario (
-idTipo INT PRIMARY KEY auto_increment,
-tipo CHAR(8)
-CONSTRAINT chkTipo CHECK (tipo IN ('Autonomo','Empresa'))
+CREATE TABLE usuario (
+idUser INT PRIMARY KEY auto_increment,
+tipo CHAR(4),
+nivelAcesso VARCHAR(45),
+email VARCHAR(45),
+senha VARCHAR(45)
 );
 
-CREATE TABLE usuario (
+CREATE TABLE empresa (
 	idUsuario INT PRIMARY KEY auto_increment,
-    nome VARCHAR(100) NOT NULL,
-    sexo CHAR (5) NOT NULL,
-    CONSTRAINT chkSexo CHECK (sexo IN ('M', 'F', 'Outro')),
-    dtNasc DATE NOT NULL,
-    CPF CHAR (11) NOT NULL,
-    RG VARCHAR (9) NOT NULL,
+    CNPJ CHAR (14) NOT NULL,
     email VARCHAR(100) NOT NULL,
     senha VARCHAR (100) NOT NULL,
-    fkTipo INT,
-    rua VARCHAR(45),
+    CEP INT,
+	endereço VARCHAR(45),
     bairro VARCHAR(45),
-    numero INT,
-    CEP CHAR(8),
-    complemento VARCHAR(45),
-    cidade VARCHAR(45),
-    estado VARCHAR(2),
-    foreign key (fkTipo) REFERENCES tipo_usuario(idTipo)
+	estado CHAR(2),
+    fkUsuario INT,
+    foreign key (fkUsuario) REFERENCES usuario(idUser)
+);
+
+CREATE TABLE telefone (
+idTelefone INT PRIMARY KEY auto_increment,
+telefone_1 VARCHAR(20) NOT NULL,
+telefone_2 VARCHAR(20),
+fkEmpresa int,
+foreign key (fkEmpresa) REFERENCES empresa(idEmpresa)
+);
+
+CREATE TABLE caminhão (
+idCaminhão INT primary key auto_increment,
+marca VARCHAR(45),
+cor varchar(45),
+placa CHAR(7),
+fkEmpresa INT,
+FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
 CREATE TABLE remessaTomates (
 idTomates INT PRIMARY KEY auto_increment,
 qnt_unidade INT,
 qnt_caixas INT,
-fkUsuario INT,
-foreign key (fkUsuario) REFERENCES usuario(idUsuario)
-);
-
-CREATE TABLE motorista (
-idMotorista INT PRIMARY KEY auto_increment,
-nome VARCHAR(45),
-dataNasc DATE,
-cpf CHAR(11),
-cnh CHAR(12)
-);
-
-CREATE TABLE trajeto (
-idTrajeto INT primary key auto_increment,
-localPartida VARCHAR(45),
-destino VARCHAR(45),
-distancia INT
-);
-
-
-CREATE TABLE caminhao (
-idCaminhao INT primary key auto_increment,
-marca VARCHAR(45),
-placa CHAR(7),
-fkMotorista INT,
-FOREIGN KEY (fkMotorista) REFERENCES motorista(idMotorista),
-fkRemessaTomate INT,
-FOREIGN KEY (fkRemessaTomate) REFERENCES remessaTomates(idTomates),
-fkTrajeto INT,
-FOREIGN KEY (fkTrajeto) REFERENCES trajeto(idTrajeto)
+qnt_quilos INT,
+fkCaminhão INT,
+foreign key (fkCaminhão) REFERENCES caminhão(idCaminhão)
 );
 
 CREATE TABLE sensor (
 idSensor INT PRIMARY KEY AUTO_INCREMENT,
-tipo CHAR(4), 
-CONSTRAINT chkTipoSensor CHECK (tipo IN ('TEMP','UMID')),
-fkCaminhao INT,
-FOREIGN KEY (fkCaminhao) REFERENCES caminhao(idCaminhao)
+tipo CHAR(4),
+fkCaminhão INT,
+FOREIGN KEY (fkCaminhão) REFERENCES caminhão(idCaminhão)
 );
 
 
@@ -82,9 +65,3 @@ umidade INT,
 fkSensor INT,
 FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor)
 );
-
-CREATE TABLE Telefone 
-	(idTelefone INT PRIMARY KEY AUTO_INCREMENT,
-    numero VARCHAR(11),
-    fkUsuario INT, FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario));
-
